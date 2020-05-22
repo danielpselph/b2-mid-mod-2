81324e9446aae107965211f0903c6de533b2bf0a
Daniel Selph <daniel.p.selph@gmail.com>
2020-05-22 10:30:05 -0600

    Add joins table for movies and actors, create model tests

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Movie, type: :model do 
   describe "relationsips" do
-    it { should belong_to(:studio) } 
     it { should have_many(:movie_actors) }
     it { should have_many(:actors).through(:movie_actors) }
   end
